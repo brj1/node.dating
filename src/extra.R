51d34a33a105e0f9c17369a31d6b958f4034556b
brj1 <brj1@sfu.ca>
2016-05-17 16:58:20 -0700

    added extra.R

---
+++ src/extra.R
@@ -0,0 +1,44 @@
+# Copyright (c) 2016, Bradley R. Jones, BC Centre for Excellence in HIV/AIDS
+# All rights reserved.
+#
+# Extra methods for node.dating
+
+# Calculate the tree log likelihood 
+#
+# t: rooted tree with edge lengths equal to genetic distance
+#
+# tip.dates: vector of dates for the tips, in the same order as t$tip.label
+#
+# mu: mutation rate
+#
+# returns the log likelihood as a double
+tree.like <- function(tree, node.dates, mu) {
+	if (mu < 0)
+		return(-Inf)
+	
+	scale.lik <- sum(-lgamma(tree$edge.length+1)+(tree$edge.length+1)*log(mu))
+		
+	calc.lik <- function(ch.node, edge, par.node) {
+		tim <- ch.node - par.node
+		
+		print(edge*log(tim)-mu*tim)
+		
+		edge*log(tim)-mu*tim
+	}
+			
+	sum(calc.lik(node.dates[tree$edge[,2]], tree$edge.length, node.dates[tree$edge[,1]])) + scale.lik
+}
+
+# Auxiliary method for plot.tree
+col.nodes <- function(t, col.tip, col.node) {
+	c(rep(col.tip, length(t$tip.label)), rep(col.node, t$Nnode))
+}
+
+# Plots a tree's node in a distance versus time
+plot.tree <- function(t, col.tip="#00aa6666", col.node="#aa660066", pch.tip=16, pch.node=5) {
+	plot(t$node.date, node.depth.edgelength(t), col=col.nides(t, col.tip, col.node), pch=pcol.nodes(t, pch.top, pch.node), xlab="Time", ylab="Genetic distance from root")
+	apply(t$edge, 1, function(e) {
+			arrows(t$node.date[e[1]], node.depth.edgelength(t)[e[1]], t$node.date[e[2]], node.depth.edgelength(t)[e[2]], col="#00000040", length=.1)
+		})
+	abline(-min(t$node.date)*t$mu, t$mu, lty=3)
+}
