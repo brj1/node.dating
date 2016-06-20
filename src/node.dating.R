f84a9f35d2698d0236f0a59ad9a258f9b2ff6f11
brj1 <brj1@sfu.ca>
2016-06-20 12:46:13 -0700

    fixed bug; fixed doc example

--- src/node.dating.R
+++ src/node.dating.R
@@ -49,7 +49,7 @@ library(ape)
 # returns the mutation rate as a double
 estimate.mu <- function(t, node.dates, p = 0.05) {
 	# fit linear model
-	g <- glm(node.depth.edgelength(t)[1:length(node.dates)] ~ tip.dates, na.action=na.omit)
+	g <- glm(node.depth.edgelength(t)[1:length(node.dates)] ~ node.dates, na.action=na.omit)
 	null.g <- glm(node.depth.edgelength(t)[1:length(node.dates)] ~ 1, na.action=na.omit)
 
 	# test fit
