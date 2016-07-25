381f42ca9c9706a9bedcd6e1644e8d503f93757d
brj1 <brj1@sfu.ca>
2016-07-25 13:04:58 -0700

    made trials shorter

--- tests/trials/trials.R
+++ tests/trials/trials.R
@@ -1,8 +1,23 @@
 library(ape)
 library(data.table)
 
-cat("generating trees...")
-trees <- lapply(unlist(lapply(4^seq(1, 6), rep, 10)), rtree)
+tree.like <- function(tree, node.dates, mu) {
+	if (mu < 0)
+		return(-Inf)
+	
+	scale.lik <- sum(-lgamma(tree$edge.length+1)+(tree$edge.length+1)*log(mu))
+		
+	calc.lik <- function(ch.node, edge, par.node) {
+		tim <- ch.node - par.node
+				
+		edge*log(tim)-mu*tim
+	}
+			
+	sum(calc.lik(node.dates[tree$edge[,2]], tree$edge.length, node.dates[tree$edge[,1]])) + scale.lik
+}
+
+cat("generating trees...\n")
+trees <- lapply(unlist(lapply(4^seq(1, 5), rep, 10)), rtree)
 
 sds <- .1^seq(1, 5)
 
@@ -10,30 +25,43 @@ rmse <- function(x, y) sqrt(sum((x-y)^2)/length(x))
 
 clock <- function(tree, rate, noise) rnorm(tree$edge.length, mean=tree$edge.length * rate, sd=noise)
 
-tab <- rbindlist(lapply(sds, function(s) {
-		cat(paste0("noise: ", s,"\n"))
+write.csv(data.frame(tips=c(), noise=c(), steps=c(), likelihood=c(), rmse=c(), time=c()), file="trial.csv")
 
-		rbindlist(lapply(trees, function(tree) {
+supress <- lapply(sds, function(s) {
+		cat(paste0("\nnoise: ", s,"\n"))
+
+		tab <- rbindlist(lapply(trees, function(tree) {
 				n <- length(tree$tip.label)
+				
+				cat(paste0(n, " "))
+				
 				s.tree <- tree
 				s.tree$edge.length <- clock(tree, 1, s)
 				tree$time <- node.depth.edgelength(s.tree)
 				tree$mu <- estimate.mu(tree, tree$time[1:n])
-				start.time <- Sys.time()
+				
 				times <- rep(0, 11)
-				dates <- estimate.dates(tree, tree$time[1:n], mu=tree$mu, opt.tol=1e-8, lik.tol=0, nsteps=0)
-				rmses <- c(rmse(dates, tree$time), rep(0, 10))
+				liks <- rep(0, 11)
+				rmses <- rep(0, 11)
+				waste.time <- 0
+				
+				start.time <- Sys.time()
+				dates <- estimate.dates(tree, tree$time[1:n], mu=tree$mu, opt.tol=1e-8, lik.tol=0, nsteps=0, is.binary=T)
 				times[1] <- Sys.time() - start.time
-								
+				
+				rmses[1] <- rmse(dates, tree$time)
+				liks[1] <- tree.like(tree, dates, tree$mu)				
+												
 				for (i in 1:10) {
-					dates <- estimate.dates(tree, dates, mu=tree$mu, opt.tol=1e-8, lik.tol=1000, nsteps=0)
+					waste.time <- (Sys.time() - start.time) - times[i]
+					dates <- estimate.dates(tree, dates, mu=tree$mu, opt.tol=1e-8, lik.tol=0, nsteps=100, is.binary=T)
+					times[i + 1] <- (Sys.time() - start.time) - waste.time
 					rmses[i + 1] <- rmse(dates, tree$time)
-					times[i + 1] <- Sys.time() - start.time
+					liks[i + 1] <- tree.like(tree, dates, tree$mu)
 				}
 				
-				data.frame(tips=rep(n, 11), noise=rep(s, 11), steps=1000*(0:10), rmse=rmses, time=times)
+				data.frame(tips=rep(n, 11), noise=rep(s, 11), steps=100*(0:10), likelihood=liks, rmse=rmses, time=times)
 			}))
-	}))
-
-cat("write...")
-write.csv(tab, file="trial.csv")
+			
+		write.csv(tab, file="trial.csv", col.names=F, row.names=F, append=T)
+	})
