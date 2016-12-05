fc6c2af2a46f0ef5617000be2551b09aa88a115b
brj1 <brj1@sfu.ca>
2016-12-05 12:26:01 -0800

    small typo

--- src/node.dating.R
+++ src/node.dating.R
@@ -56,7 +56,7 @@ estimate.mu <- function(t, node.dates, p.tol=0.05) {
 	null.g <- glm(node.depth.edgelength(t)[1:length(node.dates)] ~ 1, na.action=na.omit)
 	
 	# test fit
-	if ((1 - pchisq(AIC(null.g) - AIC(g) + 2, df=1)) > p) {
+	if ((1 - pchisq(AIC(null.g) - AIC(g) + 2, df=1)) > p.tol) {
 		warning(paste("Cannot reject null hypothesis (p=", (1 - pchisq(AIC(null.g) - AIC(g))), ")"))
 	}
 	
