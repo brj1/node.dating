2c2dc2ca94793d081b6f333aca9da6616fc24ff7
brj1 <brj1@sfu.ca>
2017-01-20 16:14:00 -0800

    fixed bug with tester.get.rmse

--- tests/simulated/tester.get.min.rmse.R
+++ tests/simulated/tester.get.min.rmse.R
@@ -7,11 +7,7 @@ mrcas <- lapply(1:n.trees, function(n) read.csv(paste0("HIV_", n, "/HIV_", n, "_
 dates <- lapply(1:n.trees, function(n) read.csv(paste0("HIV_", n, "/HIV_", n, "_dates.csv"), header=T)[,2:101])
 mrcas.node.dating <- lapply(1:n.trees, function(n) read.csv(paste0("HIV_", n, "/HIV_", n, "_mrca_node.dating.csv"), header=T)[,2:101])
 
-o <- lapply(1:n.trees, function(n) order(names(mrcas[[n]])))
-mrcas <- lapply(1:n.trees, function(n) mrcas[[n]][o[[n]], o[[n]]])
-dates <- lapply(1:n.trees, function(n) dates[[n]][o[[n]], o[[n]]])
-
-o <- lapply(1:n.trees, function(n) order(names(mrcas.node.dating[[n]])))
+o <- lapply(1:n.trees, function(n) match(names(mrcas[[n]]), gsub("(.+)_.+", "\\1", names(mrcas.node.dating[[n]]))))
 mrcas.node.dating <- lapply(1:n.trees, function(n) mrcas.node.dating[[n]][o[[n]], o[[n]]])
 
 occ <- lapply(1:n.trees, function(n) table(unlist(mrcas[[n]])))
