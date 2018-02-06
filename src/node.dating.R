dce7de5e236611f1a0b17df6e2ca440a734f5d81
brj1 <brj1@sfu.ca>
2018-02-06 13:51:50 -0800

    changed model test to anova as per Emannuel's suggestion

--- src/node.dating.R
+++ src/node.dating.R
@@ -52,11 +52,11 @@ library(ape)
 estimate.mu <- function(t, node.dates, p.tol=0.05) {
 	# fit linear model
 	g <- glm(node.depth.edgelength(t)[1:length(node.dates)] ~ node.dates, na.action=na.omit)
-	null.g <- glm(node.depth.edgelength(t)[1:length(node.dates)] ~ 1, na.action=na.omit)
+	p <- anova(g, test="Chisq")[2,5]
 	
 	# test fit
-	if ((1 - pchisq(AIC(null.g) - AIC(g) + 2, df=1)) > p.tol) {
-		warning(paste("Cannot reject null hypothesis (p=", (1 - pchisq(AIC(null.g) - AIC(g), df=1)), ")"))
+	if (p > p.tol) {
+		warning(paste("Cannot reject null hypothesis (p=", p, ")"))
 	}
 	
 	coef(g)[[2]]
