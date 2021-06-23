38badebf19bc34421b83324d69d7a3b30fd7eb20
Brad Jones <brj1@sfu.ca>
2021-06-23 12:20:56 -0700

    fixed bug in calculating bounds

--- R/node.dating.R
+++ R/node.dating.R
@@ -296,7 +296,7 @@ estimate.dates <- function(
 	get.bounds <- function(bounds) {
 		x <- c(bounds[1] + opt.tol, bounds[2] - opt.tol)
 		if (x[2] <= x[1])
-			x <- mean(bounds)
+			x <- rep(mean(bounds), 2)
 		x
 	}
 	
